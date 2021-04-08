8852d06de2ec3d3282248c242b284c6c76a6ff73
Denis <d.bulantsev@gmail.com>
2021-04-08 22:59:49 +0300

    дз3

---
+++ HW03/HW03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Ваш возраст: ");
+            byte age = Byte.Parse(Console.ReadLine());
+
+            Console.Write("Дата рождения: ");
+            string birthdate = Console.ReadLine();
+
+            Console.WriteLine(new String('-', 20));
+            Console.WriteLine("Проверка введеных данных...");
+            Console.WriteLine($"Имя: {name}\nВозраст: {age}\nДата рождения: {birthdate}");
+            Console.ReadKey();
+        }
+    }
+}
